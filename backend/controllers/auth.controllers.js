ed85d8f78dbab09624ef7fc381b8720e814f3a1e
shrash9 <shrasti.20225084@mnnit.ac.in>
2025-07-11 19:02:09 +0530

    Added login/logout controller in backend

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,3 +1,4 @@
+import genToken from "../config/token"
 import User from "../models/user.model"
 import bcrypt from "bcryptjs"
 
@@ -16,8 +17,49 @@ export const signUp = async (req,res)=>{
         const user = await User.create({
             name,password:hashedPassword,email
         })
+
+        const token = await genToken(user._id)
+        //parse token in cookie
+        res.cookie("token",token,{
+            httpOnly:true,
+            maxAge:7*24*60*60*1000,
+            sameSite:"strict",
+            secure:false
+        })
+
+        return res.status(201).json(user)
     }
     catch{
+        return res.status(500).json({message:`sign up error ${error}`})
+    }
+}
 
+
+export const Login = async (req,res)=>{
+    try{
+        const{email,password} = req.body
+
+        const user = await User.findOne({email})
+        if(!user){
+            return res.status(400).json({message:"no user wih this email!"})
+        }
+        if(password.length<6){
+            return res.status(400).json({message:"Password must be atleast 6 characters"})
+        }
+        const hashedPassword = await bcrypt.hash(password,10)
+    
+        const token = await genToken(user._id)
+        //parse token in cookie
+        res.cookie("token",token,{
+            httpOnly:true,
+            maxAge:7*24*60*60*1000,
+            sameSite:"strict",
+            secure:false
+        })
+
+        return res.status(201).json(user)
+    }
+    catch{
+        return res.status(500).json({message:`sign up error ${error}`})
     }
 }
