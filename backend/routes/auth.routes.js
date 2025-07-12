23eeec3aed480a471f1ab37739953c6a00d5f3fd
usha-here <usha.20225099@mnnit.ac.in>
2025-07-12 15:55:19 +0530

    Fix signin route path and added SignIn functionality

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -3,7 +3,7 @@ import { Login,logOut,signUp } from "../controllers/auth.controllers.js"
 
 const authRouter = express.Router()
 authRouter.post("/signup",signUp)
-authRouter.post("signin",Login)
+authRouter.post("/signin",Login)
 authRouter.get("/logout",logOut)
 
 export default authRouter
