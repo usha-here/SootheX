98214a44b927f5565b643746662036085031b556
shrash9 <shrasti.20225084@mnnit.ac.in>
2025-07-12 18:37:18 +0530

    Setup Multer

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,9 +1,21 @@
 import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs"
 
-const uploadOnCloudinary = async ()=> {
+const uploadOnCloudinary = async (filePath)=> {
      cloudinary.config({ 
         cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
         api_key: process.env.CLOUDINARY_API_KEY,
         api_secret: process.env.CLOUDINARY_API_SECRET
     });
+
+    try {
+         const uploadResult = await cloudinary.uploader.upload(filePath)
+         return uploadResult.secure_url
+         fs.unlinkSync(filePath)
+    } catch (error) {
+        fs.unlinkSync(filePath)
+        return res.status(500).json({message:"cloudinary error"})
+    }
 }
+
+export default uploadOnCloudinary
