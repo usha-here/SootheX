342651b2f7bc487ec10f417e5febf02823e5bd44
shrash9 <shrasti.20225084@mnnit.ac.in>
2025-07-14 00:04:58 +0530

    Added preset cards to choose assistant

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Card = ({image}) => {
   return (
-    <div className='w-[200px] h-[300px] bg-[#030326] border-2 border-[blue] rounded-2xl'>
+    <div className='w-[150px] h-[250px] bg-[#030326] border-2 border-[#0000ff49] rounded-2xl overflow-hidden hover:shadow-2xl hover:shadow-blue-950 cursor-pointer hover:border-4 hover:border-white'>
         <img src={image} className='h-full object-cover'></img>
     </div>
   )
--- frontend/src/pages/Customize.jsx
+++ frontend/src/pages/Customize.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import Card from '../components/Card'
+import image1 from "../assets/image1.png"
+import image2 from "../assets/image2.jpg"
+import image4 from "../assets/image4.png"
+import image3 from "../assets/authBg.png"
+import image5 from "../assets/image5.png"
+import image6 from "../assets/image6.jpeg"
+import image7 from "../assets/image7.jpeg"
 
 const Customize = () => {
   return (
-    <div className='w-full h-[100vh] bg-gradient-to-t from-[black] to-[#04049ab4]'>Customize</div>
+    <div className='w-full h-[100vh] bg-gradient-to-t from-[black] to-[#04049ab4] flex justify-center items-center flex-col'>
+      <div className='w-[90%] max-w-[60%] flex justify-center items-center flex-wrap gap-[20px]'>
+        <Card image={image1}/>
+        <Card image={image2}/>
+        <Card image={image3}/>
+        <Card image={image4}/>
+        <Card image={image5}/>
+         <Card image={image6}/>
+         <Card image={image7}/>
+      </div>
+    </div>
   )
 }
 
