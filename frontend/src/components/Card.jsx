8b77413802acf296459e11d7145bcb6a6c88fe3b
usha-here <usha.20225099@mnnit.ac.in>
2025-07-15 10:03:19 +0530

    fetched the API for the images

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -7,7 +7,13 @@ const Card = ({image}) => {
   return (
     <div className={`w-[70px] h-[140 px] lg:w-[150px] lg:h-[250px] bg-[#030326] border-2 border-[#0000ff49] 
     rounded-2xl overflow-hidden hover:shadow-2xl hover:shadow-blue-950 cursor-pointer hover:border-4
-    hover:border-white ${selectedImage==image?"border-4 border-white shadow-2xl shadow-blue-50":null}`} onClick={()=>setSelectedImage(image) }>
+    hover:border-white ${selectedImage==image?"border-4 border-white shadow-2xl shadow-blue-50":null}`} 
+    onClick={()=>{
+      setSelectedImage(image)
+      //if selected from the 7 given images, then backendImage and frontendImage shhould be null
+      setBackendImage(null)
+      setFrontendImage(null)
+      }}>
         <img src={image} className='h-full object-cover'></img>
     </div>
   )
--- frontend/src/pages/Customize2.jsx
+++ frontend/src/pages/Customize2.jsx
@@ -1,9 +1,36 @@
 import React, { useContext, useState } from 'react'
 import { userDataContext } from '../context/userContext'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom';
 
 const Customize2 = () => {
-  const {userData} = useContext(userDataContext)
-  const [assistantName,setAssistantName]=useState(userData?.assistantName||"")
+  const {userData,backendImage,selectedImage,serverUrl,setUserData} = useContext(userDataContext)
+  const [assistantName,setAssistantName]=useState(userData?.
+    assistantName||"")
+        const [loading,setLoading]=useState(false)
+
+
+    const handleUpdateAssistant=async ()=>{
+        //setLoading(true)
+        try {
+            let formData=new FormData()
+            formData.append("assistantName",assistantName)
+            if(backendImage){
+                 formData.append("assistantImage",backendImage)
+            }else{
+                formData.append("imageUrl",selectedImage)
+            }
+
+            const result=await axios.post(`${serverUrl}/api/user/update`,formData,{withCredentials:true})
+        //setLoading(false)
+            console.log(result.data)
+            setUserData(result.data)
+            //navigate("/")
+        } catch (error) {
+            //setLoading(false)
+            console.log(error)
+        }
+    }
   return (
     <div className='w-full h-[100vh] bg-gradient-to-t from-[black] to-[#04049ab4] flex justify-center items-center flex-col p-[20px] '>
       
@@ -11,17 +38,26 @@ const Customize2 = () => {
         Enter your <span className='text-blue-200'>Assistant Name</span>
       </h1>
 
+
       <input type="text" placeholder='eg:ChatGPT' className='w-full max-w-[600px] h-[60px] 
       outline-none border-2 border-white bg-transparent  text-white placeholder-gray-300 
       px-[20px] py-[10px] rounded-full text-[18px]'
-      required onChange={(e)=>setAssistantName(e.target.value)} value={assistantName}/>
+
+      required onChange={(e)=>setAssistantName(e.target.value)} 
+      value={assistantName}/>
 
       {assistantName&&<button className='min-w-[250px] h-[60px] mt-[30px] text-black font-semibold 
-        bg-white rounded-full text-[19px]'>Start your conversation!</button>}
+        bg-white rounded-full text-[19px]'  disabled={loading}
+        onClick={()=>{
+          handleUpdateAssistant()
+          }
+          }>
+          {!loading?"Start your conversation!":"Loading..."}</button>}
     </div>
     
     
   )
+
 }
 
 export default Customize2
