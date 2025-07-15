fe7f7d245f1c4a1fa2610c81f4ef2a2f5b6366e2
shrash9 <shrasti.20225084@mnnit.ac.in>
2025-07-16 01:49:47 +0530

    gemini API setup

---
+++ backend/routes/gemini.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+const geminiResponse = async(prompt) => {
+    try {
+        const apiUrl = process.env.GEMINI_API_URL;
+        const result = await axios.post(apiUrl, {
+            "contents" : [{
+                "parts":[{"text": prompt}]
+            }]
+        })
+        return result.data;
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export default geminiResponse;
