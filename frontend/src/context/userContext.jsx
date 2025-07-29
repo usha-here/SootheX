050695dac8d7a0ba89b7b1015341e387f4499b5b
usha here <usha.20225099@mnnit.ac.in>
2025-07-30 05:27:45 +0530

    Update userContext.jsx

--- frontend/src/context/userContext.jsx
+++ frontend/src/context/userContext.jsx
@@ -6,7 +6,7 @@ import axios from 'axios';
 
 export const userDataContext = createContext();
 const UserContext = ({children}) => {
-  const serverUrl="http://localhost:8000"
+  const serverUrl="https://soothex-backend.onrender.com"
   const [userData,setUserData] = useState(null)
   const [frontendImage, setFrontendImage] = useState(null)
     const [backendImage, setBackendImage] = useState(null)
